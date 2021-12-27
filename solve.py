e6e3756fb90ae8ef9d15ebfe5229b7b617b6bca0
eliot <17310184+EliotNapa@users.noreply.github.com>
2021-12-27 13:31:21 +0900

    question solved

---
+++ solve.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+"""
+urban hack解法プログラム
+"""
+import copy
+
+class CalcRecord():
+    """
+    逆ポーランド用の要素格納クラス
+    """
+    def __init__(self, a,b,c,d,op1,op2,op3):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.op1 = op1
+        self.op2 = op2
+        self.op3 = op3
+        self.result = -1
+
+
+
+
+def main():
+    """
+    urban hacks解法総当り計算
+    """
+    #問題文数字リスト
+    src_nums = ["1", "3", "3", "7"]
+    #src_nums = ["1", "2", "3", "4"]
+
+    #演算子リスト
+    op_list = ["+", "-", "*", "/"]
+
+    #順列ホルダー
+    nums_holder = [0] * len(src_nums)
+
+    #全順列格納用リスト
+    result_list = []
+
+    #順列を作成する
+    process_ary(src_nums, 0, len(src_nums), nums_holder, result_list)
+
+    #逆ポーランド用スタック
+    calc_list = []
+
+    #演算子の順列を作成する
+    for i in result_list:
+        for op1 in op_list:
+            for op2 in op_list:
+                for op3 in op_list:
+                    calc_list.append(CalcRecord(
+                        i[0],
+                        i[1],
+                        i[2],
+                        i[3],
+                        op1,
+                        op2,
+                        op3
+                    ))
+
+    calc_work = []
+
+
+
+    for cur_calc in calc_list:
+        calc_work.clear()
+        #逆ポーランド演算式1
+        # A B op1 C D op2 op3
+        # (A op1 B) op3 (C op2 D)
+        #1 7 / 3 3 + * = 10.0
+        #(1 / 7)*(3 + 3)
+        #
+        # (1 + (7 / 3)) * 3
+        # 1 7 3 / + 3 *
+        
+        calc_work.insert(0, cur_calc.a)
+        calc_work.insert(0, cur_calc.b)
+        exec_revpol(cur_calc.op1, calc_work)
+        calc_work.insert(0, cur_calc.c)
+        calc_work.insert(0, cur_calc.d)
+        exec_revpol(cur_calc.op2, calc_work)
+        exec_revpol(cur_calc.op3, calc_work)
+        ans = calc_work.pop()
+        print("F1#{0} {1} {2} {3} {4} {5} {6} = {7}".format(
+            cur_calc.a,
+            cur_calc.b,
+            cur_calc.op1,
+            cur_calc.c,
+            cur_calc.d,
+            cur_calc.op2,
+            cur_calc.op3,
+            ans
+        ))
+        if 10 == ans:
+            return
+
+        calc_work.clear()
+        #逆ポーランド演算式2
+        # A B op1 C op2 D op3
+        # ((A op1 B) op2 C) op3 D
+        calc_work.insert(0, cur_calc.a)
+        calc_work.insert(0, cur_calc.b)
+        exec_revpol(cur_calc.op1, calc_work)
+        calc_work.insert(0, cur_calc.c)
+        exec_revpol(cur_calc.op2, calc_work)
+        calc_work.insert(0, cur_calc.d)
+        exec_revpol(cur_calc.op3, calc_work)
+        ans = calc_work.pop()
+        print("F2#{0} {1} {2} {3} {4} {5} {6} = {7}".format(
+            cur_calc.a,
+            cur_calc.b,
+            cur_calc.op1,
+            cur_calc.c,
+            cur_calc.op2,
+            cur_calc.d,
+            cur_calc.op3,
+            ans
+        ))
+        if 10 == ans:
+            return
+
+        calc_work.clear()
+        #逆ポーランド演算式3
+        # A B C op1 op2 D op3
+        # (A op2 (B op1 C)) op3 D
+        calc_work.insert(0, cur_calc.a)
+        calc_work.insert(0, cur_calc.b)
+        calc_work.insert(0, cur_calc.c)
+        exec_revpol(cur_calc.op1, calc_work)
+        exec_revpol(cur_calc.op2, calc_work)
+        calc_work.insert(0, cur_calc.d)
+        exec_revpol(cur_calc.op3, calc_work)
+        ans = calc_work.pop()
+        print("F3#{0} {1} {2} {3} {4} {5} {6} = {7}".format(
+            cur_calc.a,
+            cur_calc.b,
+            cur_calc.c,
+            cur_calc.op1,
+            cur_calc.op2,
+            cur_calc.d,
+            cur_calc.op3,
+            ans
+        ))
+        if 10 == ans:
+            return
+
+        calc_work.clear()
+        #逆ポーランド演算式4
+        # A B C D op1 op2 op3
+        # A op3 (B op2 (C op1 D))
+        calc_work.insert(0, cur_calc.a)
+        calc_work.insert(0, cur_calc.b)
+        calc_work.insert(0, cur_calc.c)
+        calc_work.insert(0, cur_calc.d)
+        exec_revpol(cur_calc.op1, calc_work)
+        exec_revpol(cur_calc.op2, calc_work)
+        exec_revpol(cur_calc.op3, calc_work)
+        ans = calc_work.pop()
+        print("F4#{0} {1} {2} {3} {4} {5} {6} = {7}".format(
+            cur_calc.a,
+            cur_calc.b,
+            cur_calc.c,
+            cur_calc.d,
+            cur_calc.op1,
+            cur_calc.op2,
+            cur_calc.op3,
+            ans
+        ))
+        if 10 == ans:
+            return
+
+
+def exec_revpol(op, work_list):
+    """
+    逆ポーランド演算を実施する
+    op        - 演算子
+    work_list - 演算の要素と結果を格納するスタック
+    """
+    s2 = work_list.pop(0)
+    s1 = work_list.pop(0)
+    if (s1 == "∞" or s2 == "∞"):
+        work_list.insert(0, "∞")
+        return
+
+    r2 = float(s2)
+    r1 = float(s1)
+    if op == "+":
+        # print("{0} {1} {2} = {3}".format(
+        #     r1,
+        #     op,
+        #     r2,
+        #     r1 + r2
+        # ))
+        work_list.insert(0, r1 + r2)
+    elif  op == "-":
+        # print("{0} {1} {2} = {3}".format(
+        #     r1,
+        #     op,
+        #     r2,
+        #     r1 - r2
+        # ))
+        work_list.insert(0, r1 - r2)
+    elif  op == "*":
+        # print("{0} {1} {2} = {3}".format(
+        #     r1,
+        #     op,
+        #     r2,
+        #     r1 * r2
+        # ))
+        work_list.insert(0, r1 * r2)
+    elif  op == "/":
+        if (0 == r2):
+            # print("{0} {1} {2} = {3}".format(
+            #     r1,
+            #     op,
+            #     r2,
+            #     "∞"
+            # ))
+            work_list.insert(0, "∞")
+        else:
+            # print("{0} {1} {2} = {3}".format(
+            #     r1,
+            #     op,
+            #     r2,
+            #     r1 / r2
+            # ))
+            work_list.insert(0, r1 / r2)
+
+def process_ary(src_list, depth, org_depth, nums_holder, result_list):
+    """
+    数字の順列作成(再帰)
+    src_list     - 問題数字列(呼び元で選択されたものは削除されていく)
+    depth        - 再帰深さ
+    org_depth    - 元文字列の長さ
+    nums_holder  - 順列格納用
+    result_list  - すべての順列を格納するリスト
+    """
+    for idx in range(len(src_list)):
+        local_list = copy.copy(src_list)
+        nums_holder[depth] = local_list.pop(idx)
+        if depth < org_depth - 1:
+            process_ary(local_list, depth + 1, org_depth, nums_holder, result_list)
+        else:
+            # print("A:{0},B:{1},C:{2},D:{3}".format(nums_holder[0],nums_holder[1],nums_holder[2],nums_holder[3]))
+            result_list.append([nums_holder[0],nums_holder[1],nums_holder[2],nums_holder[3]])
+
+
+if __name__ == '__main__':
+    main()
+
